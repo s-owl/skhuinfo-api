4b5243cc8f8920721e7beb3d8033b36ca4ffd737
tpdns90321 <tpdns9032100@gmail.com>
2020-08-16 13:14:24 +0900

    [meal] meal/ids 테스트 코드 작성
    1. 타겟 홈페이지와 비슷한 모킹 데이터 구성
    2. 그것을 사용하는 테스트 코드 작성

---
+++ meal_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var MealHttpMock HttpClient = &HttpMock{
+	map[string]string{
+		MEAL_LIST: "test/meal_list.html",
+	},
+}
+
+func Test_getMealId(t *testing.T) {
+	assert := assert.New(t)
+	if list, err := getMealID(MealHttpMock); err != nil {
+		t.Fatal(err)
+	} else {
+		assert.Equal(
+			MealID{
+				ID:    389,
+				Title: "학생식당 주간메뉴입니다(12/2-12/6)",
+				Date:  "2019-11-29",
+			},
+			list[0],
+		)
+	}
+}
